0152d643fde7a13610163bc1f56a5a7adf9dcfac
Biak18 <biakceu912@gmail.com>
2026-07-24 15:09:11 +0630

    d

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -87,10 +87,7 @@ export function Dialog({
             transition={{ duration: 0.35, ease: [0.22, 1, 0.36, 1] }}
             className="fixed inset-y-0 right-0 z-50 w-full max-w-full sm:max-w-xl"
           >
-            <div
-              ref={panelRef}
-              className="flex h-full min-w-0 flex-col overflow-y-auto overscroll-contain bg-background sm:border-l sm:border-border"
-            >
+            <div ref={panelRef}>
               <div className="relative flex h-full min-w-0 flex-col overflow-y-auto overscroll-contain bg-background sm:border-l sm:border-border">
                 <button
                   ref={closeButtonRef}
@@ -100,7 +97,7 @@ export function Dialog({
                 >
                   <X size={18} />
                 </button>
-                <div className="min-w-0 wrap-break-word px-6 pb-16 pt-16 sm:px-8">
+                <div className="flex-wrap px-6 pb-16 pt-16 sm:px-8">
                   {children}
                 </div>
               </div>
